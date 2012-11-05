187487abe50c9d68374b05414f97091bbda095b6
Kumaresan <kmanickavelu@ebay.com>
2012-11-05 16:34:21 +0530

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 DemoApp::Application.routes.draw do
+
+  root to: 'static_pages#home'
+
   get "static_pages/home"
 
   get "static_pages/help"
 
   get "static_pages/about"
+
+  get "static_pages/contact"
 
   resources :microposts
 
